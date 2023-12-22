4e2596bcad5f57e7ff36b229bbd0e39eae34f376
caiohxp <caiocesar.ccomp@gmail.com>
2023-12-22 02:26:43 -0300

    clickgokudrip finish

---
+++ clickgokudrip/scripts/engine.js
@@ -0,0 +1,59 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    value: {
+        timerId: null,
+        gameSpeed: 1000,
+        hitPosition: null,
+        result: 0,
+        currentTime: 60,
+    }
+};
+
+function timer(){
+    setInterval(() => {
+        if(state.value.currentTime > 0) state.value.currentTime--;
+        else alert(`Game Over! O seu resultado foi ${state.value.result}`);
+        state.view.timeLeft.textContent = state.value.currentTime;
+    },1000)
+}
+
+function main() {
+    moveEnemy();
+    addListenerHitbox();
+    timer();
+}
+
+function randomSquare() {
+    let randomNumber = Math.floor(Math.random() * 9);
+    state.view.squares.forEach((square, i) => {
+        square.classList.remove("enemy");
+        if (i == randomNumber) {
+            square.classList.add("enemy");
+            state.value.hitPosition = square.id;
+        }
+    })
+    console.log(randomNumber);
+}
+
+function addListenerHitbox() {
+    state.view.squares.forEach(square => {
+        square.addEventListener("mousedown", () => {
+            if (state.value.hitPosition === square.id) {
+                state.value.result++;
+                state.view.score.textContent = state.value.result;
+                state.values.hitPosition = null;
+            }
+        })
+    })
+}
+
+function moveEnemy() {
+    state.value.timerId = setInterval(randomSquare, state.value.gameSpeed);
+}
+
+main();
